710f16858c05e6368f5aee8c0d789b29e2607b7e
LeoHagi <leo271@icloud.com>
2023-08-02 17:16:45 +0900

    firebaseに対応するように更新。要エラーハンドリング実装。

--- project/lib/src/login/login_page.dart
+++ project/lib/src/login/login_page.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
-
-import 'static_email_data.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:project/packages.dart';
+import 'static_email_data.dart'; // check if the email is valid staticaly
+import 'account_manager_firebase.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -11,6 +12,14 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,30 +28,93 @@ class _LoginPageState extends State<LoginPage> {
         title: const Text("Login Page"),
       ),
       body: Center(
-          child: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            TextFormField(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return "メールアドレスが空です";
-                }
-                return emailValidator(value);
-              },
-              decoration: const InputDecoration(
-                hintText: "メールアドレスを入力してください",
-              ),
-            ),
-            ElevatedButton(
-                onPressed: () => {
-                      if (_formKey.currentState != null &&
-                          _formKey.currentState!.validate())
-                        {Navigator.pushNamed(context, '/home/profile')}
+          child: Column(
+        children: [
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                // Form for the email address
+                TextFormField(
+                  controller: _emailController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "メールアドレスが空です";
+                    }
+                    return emailValidator(value);
+                  },
+                  decoration: const InputDecoration(
+                    hintText: "メールアドレスを入力してください",
+                  ),
+                ),
+
+                // Form for the password
+                TextFormField(
+                  controller: _passwordController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "パスワードが空です";
+                    }
+                    return null;
+                  },
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: "パスワードを入力してください",
+                  ),
+                ),
+
+                ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState == null ||
+                          !_formKey.currentState!.validate()) {
+                        return;
+                      }
+                      // here the email and password is guaranteed to be valid form
+                      // check if the email is registered
+                      final email = _emailController.text;
+                      final password = _passwordController.text;
+                      try {
+                        final user = (await signInWithEmail(email, password))!;
+                        if (user.emailVerified) {
+                          if (mounted) {
+                            Navigator.pushNamed(context, '/home/profile',
+                                arguments: user);
+                          }
+                        } else {
+                          // TODO: implement error handling
+                          errorLog('Email is not verified');
+                        }
+                      } on FirebaseAuthException catch (e) {
+                        // TODO: implement error handling
+                        switch (e.code) {
+                          case 'user-not-found':
+                            errorLog('No user found for that email.');
+                            break;
+                          case 'wrong-password':
+                            errorLog('Wrong password provided for that user.');
+                            break;
+                          case 'user-disabled':
+                            errorLog(
+                                'The user corresponding to the given email has been disabled.');
+                            break;
+                          case 'network-request-failed':
+                            errorLog(
+                                'Network error (such as timeout, interrupted connection or unreachable host) has occurred.');
+                            break;
+                          default:
+                            errorLog(e.code);
+                        }
+                      }
                     },
-                child: const Text("Submit"))
-          ],
-        ),
+                    child: const Text("ログインする"))
+              ],
+            ),
+          ),
+          const SizedBox(height: 50),
+          ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/login/signup'),
+              child: const Text("アカウントを作成する"))
+        ],
       )),
     );
   }
