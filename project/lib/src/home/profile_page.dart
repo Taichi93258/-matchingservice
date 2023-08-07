1a2e52feebeea12fa9e817fdb668f66c30550843
LeoHagi <leo271@icloud.com>
2023-08-07 18:30:44 +0900

    同時に複数の変更ができないようにしました。

--- project/lib/src/home/profile_page.dart
+++ project/lib/src/home/profile_page.dart
@@ -1,7 +1,7 @@
 import 'package:project/packages.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:project/src/home/user_database.dart';
-import 'package:project/logic/editable_text_field.dart';
+import 'package:project/src/home/editable_text_field.dart';
 import 'profile.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -12,78 +12,54 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  int editingFlag = 0;
+
   @override
   Widget build(BuildContext context) {
     final User? user = ModalRoute.of(context)!.settings.arguments as User?;
-    final Profile profile =
-        user != null ? UserDatabase.search(user.email!) : Profile.master();
+    final List<String> profile = user != null
+        ? UserDatabase.search(user.email!).indexed
+        : Profile.master().indexed;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Profile Page"),
-        automaticallyImplyLeading: false,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: ListView(
-          children: [
-            EditableTextField(
-              initialText: profile.name,
-              onSubmitted: (value) {
-                setState(() {
-                  profile.name = value;
-                });
-              },
-            ),
-            EditableTextField(
-              initialText: profile.university,
-              onSubmitted: (value) {
-                setState(() {
-                  profile.university = value;
-                });
-              },
-            ),
-            EditableTextField(
-              initialText: profile.faculty,
-              onSubmitted: (value) {
-                setState(() {
-                  profile.faculty = value;
-                });
-              },
-            ),
-            EditableTextField(
-              initialText: profile.department,
-              onSubmitted: (value) {
-                setState(() {
-                  profile.department = value;
-                });
-              },
-            ),
-            EditableTextField(
-              initialText: profile.grade,
-              onSubmitted: (value) {
-                setState(() {
-                  profile.grade = value;
-                });
-              },
-            ),
-            EditableTextField(
-                editable: false, initialText: profile.birthdayFormatted),
-            const SizedBox(
-              height: 4,
-            ),
-            EditableTextField(
-              initialText: profile.introduction,
-              onSubmitted: (value) {
-                setState(() {
-                  profile.introduction = value;
-                });
-              },
-            ),
-            Image.network(profile.imageURL),
-          ],
+        appBar: AppBar(
+          title: const Text("Profile Page"),
+          automaticallyImplyLeading: false,
         ),
-      ),
-    );
+        body: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+                children: List.generate(Profile.dataLength, (index) {
+              if (index == Profile.getId('imageURL')) {
+                return Image.network(
+                    'https://img.freepik.com/free-photo/orthodontics-dental-care-and-stomatology-concept-close-up-of-gloomy-and-sad-cute-asian-guy-sulking-looking-upset-at-camera-feeling-lonely-or-distressed-standing-white-background-in-bad-mood_1258-57140.jpg?size=626&ext=jpg');
+              } else if (index == Profile.getId('name') ||
+                  index == Profile.getId('introduction')) {
+                return EditableTextField(
+                  initialText: profile[index],
+                  multiline: index == Profile.getId('introduction'),
+                  onSubmitted: (value) {
+                    setState(() {
+                      profile[index] = value;
+                      editingFlag = -1;
+                    });
+                  },
+                  isEditing: editingFlag == index,
+                  onFocused: () {
+                    infoLog('onFocused: $index');
+                    setState(() {
+                      editingFlag = index;
+                    });
+                    infoLog('editingFlag: $editingFlag');
+                    infoLog('isEditing: ${editingFlag == index} at $index');
+                  },
+                );
+              } else {
+                return EditableTextField(
+                  initialText: profile[index],
+                  editable: false,
+                );
+              }
+            }))));
   }
 }
